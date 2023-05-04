129a5e8bb968956771f25f0fa9c94d2f3f59bd47
Mgrdich <mgotm13@gmail.com>
2023-05-04 17:21:37 +0400

    HTML and Windows code modfications

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('Electron App');
+const {app, BrowserWindow} = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    return win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) return createWindow()
+    });
+
+    return createWindow();
+});
+
+
+app.on('window-all-closed', () => {
+    // Windows and Linux will actually close the application
+    if (process.platform !== 'darwin') app.quit()
+});
